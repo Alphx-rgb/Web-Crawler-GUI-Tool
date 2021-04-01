45d0105096b1edaefd95babbc0110f0725ced29b
Alphx-rgb <78094309+Alphx-rgb@users.noreply.github.com>
2021-04-01 16:43:08 +0530

    Update Readme: Basic_Crawler.py

--- Readme: Basic_Crawler.py
+++ Readme: Basic_Crawler.py
@@ -1,3 +1,5 @@
 The program will gather all the links,headers and images url and store them in separate files.
 I have use voice modulation to make it look more interactive.
 
+I need to improve my code for crawling dynamic websites(made of JS scripts)
+I may also add threading to improve its speed.
