4b5a5e93e48833dc38e6692675ca7d6c795ae8fc
Alphx-rgb <78094309+Alphx-rgb@users.noreply.github.com>
2021-04-10 23:06:26 +0530

    Update WCSC.py

--- WCSC.py
+++ WCSC.py
@@ -131,7 +131,7 @@ else:
     depth=1
    
 start(url)
-burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"}
+burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36","Accept-Encoding": "gzip, deflate"}
 client=requests.get(url,headers=burp0_headers)
 soup=BeautifulSoup(client.text)
 lst=list()
