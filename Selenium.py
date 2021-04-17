363b64fd5de48ff5f43afe3c23b8be1c097e3c11
Alphx-rgb <78094309+Alphx-rgb@users.noreply.github.com>
2021-04-17 15:05:09 +0530

    Update Selenium.py

--- Selenium.py
+++ Selenium.py
@@ -16,10 +16,10 @@ def urll(website,urls,strng,c):
         if(name!=None):
             cprint(k,"green",end="")
             print(".",end="")
-            cprint(name,"cyan",end="")
+            cprint(name.replace("\n","").strip(),"cyan",end="")
             print(".....")
         else:
-            name = Url
+            name = (((Url.strip()).split("."))[0]).replace("/"," ")
             cprint(k,"green",end="")
             print(".",end="")
             cprint(name,"cyan",end="")
@@ -36,9 +36,9 @@ def urll(website,urls,strng,c):
             driver.maximize_window()
             sleep(2)
             if("." not in name):
-                driver.save_screenshot(os.getcwd() + "\\Screen-Shots\\" + name + str(k) +"_ss.png")
+                driver.save_screenshot(os.getcwd() + "\\Screen-Shots\\" + (name.replace("\n","").strip()).replace("/"," ") + str(k) +"_ss.png")
             else:
-                driver.save_screenshot(os.getcwd()+"\\Screen-Shots\\" + (name.split("."))[1] + "_" + str(k) +"_ss.png")
+                driver.save_screenshot(os.getcwd()+"\\Screen-Shots\\" + ((name.split("."))[1]).strip() + "_" + str(k) +"_ss.png")
             print("Next...")
         except:
             pass
